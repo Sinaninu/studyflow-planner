c6f59789c0e238656aff06295f25a1b4063e9c02
Sina Mussie <your.email@example.com>
2026-04-23 17:06:20 +0200

    feature: add task routes

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.json({message:'StudyFlow Planner is running!'});
 });
+
+app.use("/api/tasks", taskRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
